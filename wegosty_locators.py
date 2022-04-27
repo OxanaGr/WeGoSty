542a02cdac192c08aedc735e6d3a93c68bb8512b
OxanaGr <100324401+OxanaGr@users.noreply.github.com>
2022-04-27 12:10:56 -0700

    Update wegosty_locators.py

--- wegosty_locators.py
+++ wegosty_locators.py
@@ -3,19 +3,13 @@ fake = Faker(locale='en_CA')
 
 app = 'WeGoStudy'
 wegosty_url = 'http://34.233.225.85/students/admissions'
+wegosty_homepage_url = 'http://34.233.225.85/'
 werosty_home_page_title = ''
 first_name = fake.first_name()
 last_name = fake.last_name()
-passport_number = fake.pyint(1111111, 9999999)
+passport_number = f'Pa{fake.pyint(11, 9999)}'
 phone_number = fake.phone_number()
-# apartment_number =
 mailing_address = fake.street_address()
 user_email = fake.email()
-date_of_birth = fake.date()
-# province_state =
-# city =
 postal_code = fake.postalcode()
-# user_email =
-# school_name =
-# program =
-# gpa =
+date_of_birth = fake.date()
