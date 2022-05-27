2763f1cef928e7157c109bf0159b2e026114e1c2
OxanaGr <100324401+OxanaGr@users.noreply.github.com>
2022-05-26 17:55:39 -0700

    Update wegosty_locators.py

--- wegosty_locators.py
+++ wegosty_locators.py
@@ -2,16 +2,31 @@ from faker import Faker
 fake = Faker(locale='en_CA')
 
 app = 'WeGoStudy'
-wegosty_url = 'http://34.233.225.85/students/admissions'
+partner_name = 'xxxxx'
+partner_pass = 'xxxxxxx!'
+# wegosty_url = 'http://34.233.225.85/students/admissions'
+wegosty_url = 'https://wegostudy.ca/partner/home'
 wegosty_homepage_url = 'http://34.233.225.85/'
 werosty_home_page_title = ''
 first_name = fake.first_name()
 last_name = fake.last_name()
-passport_number = f'Pa{fake.pyint(11, 9999)}'
+passport_number = f'PA{fake.random_int(100000, 999999)}'
 phone_number = fake.phone_number()
+# apartment_number =
 mailing_address = fake.street_address()
 user_email = fake.email()
+college_ID = fake.random_int(10000, 99999)
+student_permit_number = f'SP{fake.random_int(10000, 99999)}'
+first_language = 'English'
+# school = 'Douglas College'
+
+# province_state =
+# city =
 postal_code = fake.postalcode()
+# user_email =
+# school_name =
+# program =
+# gpa =
 date_of_birth = fake.date_of_birth(minimum_age = 16)
 
 
